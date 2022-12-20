a2008b7238b2f2f5405d851555aae2cab981d541
16underscore <steffen.gminsky@online.de>
2022-12-20 19:41:18 +0100

    Use sort entrypoint

--- src/main/java/me/sixteen_/sort_extension/SortExtension.java
+++ src/main/java/me/sixteen_/sort_extension/SortExtension.java
@@ -1,19 +1,19 @@
 package me.sixteen_.sort_extension;
 
 import me.sixteen_.sort.Sort;
+import me.sixteen_.sort.api.SortClientModInitializer;
 import me.sixteen_.sort_extension.sort_impl.Config;
 import me.sixteen_.sort_extension.sort_impl.Order;
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
-public class SortExtension implements ClientModInitializer {
+public class SortExtension implements SortClientModInitializer {
 
 	private static SortExtension instance;
 	private Config config;
 	private Order order;
 
 	@Override
-	public void onInitializeClient() {
+	public void onInitializeSortClient() {
 		instance = this;
 		config = new Config();
 		order = new Order();
